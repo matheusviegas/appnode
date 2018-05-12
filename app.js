0c72073a9cbe6835505fe143c30eb079f9efe898
Matheus Souza <matheusviegasdesouza@gmail.com>
2018-05-12 14:31:13 -0300

    Corrigindo indentação do código para melhor visualização

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function(request, response) {
 
-response.writeHead(
-200,
-{"Content-Type":"text/html"}
-);
+    response.writeHead(
+        200, {
+            "Content-Type": "text/html"
+        }
+    );
 
-if(request.url ==='/'){
-response.write('<h1>Meu APP JS</h1>');
-} else if (request.url === '/pos'){
-response.write('<h1>SENAC</h1>');
-} else {
-response.write('<h1>Erro de rota</h1>');
-}
+    if (request.url === '/') {
+        response.write('<h1>Meu App NodeJS</h1>');
+    } else if (request.url === '/pos') {
+        response.write('<h1>Pós Graduação SENAC</h1>');
+    } else {
+        response.write('<h1>Erro de rota</h1>');
+    }
 
-response.end();
-	
+    response.end();
+
 });
 
-server.listen(8080, function(){
-console.log("Servidor rodando! Acesse: http://localhost:8080");
+server.listen(8080, function() {
+    console.log("Servidor rodando! Acesse: http://localhost:8080");
 });
